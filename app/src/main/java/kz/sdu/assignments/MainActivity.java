a5170376ae9dad69829a76350946db92f5618e4f
AlphaKennyBudy <mr.adil777@gmail.com>
2020-09-08 23:31:04 +0600

    toggle button text has been added

--- app/src/main/java/kz/sdu/assignments/MainActivity.java
+++ app/src/main/java/kz/sdu/assignments/MainActivity.java
@@ -2,13 +2,30 @@ package kz.sdu.assignments;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.res.Resources;
+import android.icu.text.IDNA;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private boolean isTextVisible = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void toggleText(View view) {
+        TextView InfoText = (TextView) findViewById(R.id.info);
+        if (!isTextVisible) {
+            Resources resourses = getResources();
+            String text = resourses.getString((R.string.name)) + "\n" + resourses.getString(R.string.surname) + "\n" + resourses.getString(R.string.ID);
+            InfoText.setText(text);
+        } else {
+            InfoText.setText("");
+        }
+        isTextVisible = !isTextVisible;
     }
 }
