113eb990f9d7377664fe578431bd0824ad7eaa87
AlphaKennyBudy <mr.adil777@gmail.com>
2020-09-08 23:54:03 +0600

    ass2 completed

--- app/src/main/java/kz/sdu/assignments/MainActivity.java
+++ app/src/main/java/kz/sdu/assignments/MainActivity.java
@@ -6,15 +6,24 @@ import android.content.res.Resources;
 import android.icu.text.IDNA;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
     private boolean isTextVisible = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Spinner spinner = (Spinner) findViewById(R.id.product_spinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.planets_array, android.R.layout.simple_spinner_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(this);
     }
 
     public void toggleText(View view) {
@@ -27,5 +36,17 @@ public class MainActivity extends AppCompatActivity {
             InfoText.setText("");
         }
         isTextVisible = !isTextVisible;
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        TextView ProductText = (TextView) findViewById(R.id.best_product);
+        ProductText.setText(String.valueOf(position + 1));
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+        TextView ProductText = (TextView) findViewById(R.id.best_product);
+        ProductText.setText("");
     }
 }
