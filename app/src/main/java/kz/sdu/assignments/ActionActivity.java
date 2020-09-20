333e1bf78b0313bf3aa3b715a27062b850becf49
AlphaKennyBudy <mr.adil777@gmail.com>
2020-09-20 20:23:34 +0600

    2 more activities added

---
+++ app/src/main/java/kz/sdu/assignments/ActionActivity.java
@@ -0,0 +1,14 @@
+package kz.sdu.assignments;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ActionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_action);
+    }
+}
---
+++ app/src/main/java/kz/sdu/assignments/FinishActivity.java
@@ -0,0 +1,14 @@
+package kz.sdu.assignments;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FinishActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_finish);
+    }
+}
